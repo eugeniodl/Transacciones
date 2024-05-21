50af04bcad6a3d82b6bde002fa40915588f99677
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-05-21 12:20:15 -0600

    Ejemplo02

---
+++ Ejemplo02/Migrations/Bank2/20240521172255_ini2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ejemplo02.Migrations.Bank2
+{
+    /// <inheritdoc />
+    public partial class ini2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Credit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Debit = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTransactions", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AccountTransactions",
+                columns: new[] { "Id", "AccountNumber", "Credit", "Debit" },
+                values: new object[] { 1, "20", 400m, 0m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountTransactions");
+        }
+    }
+}
