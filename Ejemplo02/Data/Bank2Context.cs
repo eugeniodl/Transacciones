b0e3669aa024e1ba61288c0dfd0fd26186c797f1
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-05-21 08:37:04 -0600

    Ejemplo02

---
+++ Ejemplo02/Data/Bank2Context.cs
@@ -0,0 +1,45 @@
+﻿using Ejemplo02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo02.Data
+{
+    public class Bank2Context : DbContext
+    {
+        public DbSet<AccountTransaction> AccountTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configurar la cadena de conexión para Bank2Context
+            string connection =
+                ConfigurationManager.ConnectionStrings["constring2"].ConnectionString;
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountTransaction>().HasData(
+                new AccountTransaction()
+                {
+                    Id = 1,
+                    AccountNumber = "20",
+                    Credit = 400M,
+                    Debit = 0M
+                });
+
+            modelBuilder.Entity<AccountTransaction>()
+                .Property(x => x.Credit)
+                .HasColumnType("decimal(18,2)");
+
+
+            modelBuilder.Entity<AccountTransaction>()
+                .Property(x => x.Debit)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
